b01af2947fb9bb698cd6f6e13c706a554481e852
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2014-06-16 23:20:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocketshark::Application.config.secret_token = '4c36087c19fd24cec8c1c8a23f1094ccfc923ab9eb0065df823422704119b0fdedb99b28cc392dbe60a455b3b2c86082d2f3b4f74c02efcaf833757200e4c1dc'
